de15306f13b27b066ecb65d7fa29db7f4e61fde4
z789 <cfcjl@163.com>
2017-03-29 19:32:00 +0800

    macro instead of for loop, improve the performance 40%.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
 all:
-	gcc -Wall -o sha3sum sha3sum.c sha3.c
+	gcc -Wall -DKECCAKF_MACRO -o sha3sum sha3sum.c sha3.c
 clean:
 	rm -f sha3sum
--- sha3.c
+++ sha3.c
@@ -27,8 +27,146 @@ static const int keccakf_piln[24] = {
 	15, 23, 19, 13, 12, 2, 20, 14, 22, 9,  6,  1
 };
 
-/* update the state with given number of rounds */
+#ifdef KECCAKF_MACRO
 
+#define TIT(st, bc)                                               \
+	do {                                                      \
+		bc[0] = st[0] ^ st[5] ^ st[10] ^ st[15] ^ st[20]; \
+		bc[1] = st[1] ^ st[6] ^ st[11] ^ st[16] ^ st[21]; \
+		bc[2] = st[2] ^ st[7] ^ st[12] ^ st[17] ^ st[22]; \
+		bc[3] = st[3] ^ st[8] ^ st[13] ^ st[18] ^ st[23]; \
+		bc[4] = st[4] ^ st[9] ^ st[14] ^ st[19] ^ st[24]; \
+	} while (0)
+
+#define TST(st, i, t)                                             \
+	do {                                                      \
+		st[0 + i] ^= t;                                   \
+		st[5 + i] ^= t;                                   \
+		st[10 + i] ^= t;                                  \
+		st[15 + i] ^= t;                                  \
+		st[20 + i] ^= t;                                  \
+	} while (0)
+
+#define TBC(st, bc)                                               \
+	do {                                                      \
+		TST(st, 0, (bc[4] ^ ROTL64(bc[1], 1)));           \
+		TST(st, 1, (bc[0] ^ ROTL64(bc[2], 1)));           \
+		TST(st, 2, (bc[1] ^ ROTL64(bc[3], 1)));           \
+		TST(st, 3, (bc[2] ^ ROTL64(bc[4], 1)));           \
+		TST(st, 4, (bc[3] ^ ROTL64(bc[0], 1)));           \
+	} while (0)
+
+#define RP(st, bc)                                                \
+	do {                                                      \
+		int j;                                            \
+		uint64_t t = st[1];                               \
+		j = keccakf_piln[0]; bc[0] = st[j];               \
+                st[j] = ROTL64(t, keccakf_rotc[0]); t = bc[0];    \
+		j = keccakf_piln[1]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[1]); t = bc[0];    \
+		j = keccakf_piln[2]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[2]); t = bc[0];    \
+		j = keccakf_piln[3]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[3]); t = bc[0];    \
+		j = keccakf_piln[4]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[4]); t = bc[0];    \
+		j = keccakf_piln[5]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[5]); t = bc[0];    \
+		j = keccakf_piln[6]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[6]); t = bc[0];    \
+		j = keccakf_piln[7]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[7]); t = bc[0];    \
+		j = keccakf_piln[8]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[8]); t = bc[0];    \
+		j = keccakf_piln[9]; bc[0] = st[j];               \
+		st[j] = ROTL64(t, keccakf_rotc[9]); t = bc[0];    \
+		j = keccakf_piln[10]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[10]); t = bc[0];   \
+		j = keccakf_piln[11]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[11]); t = bc[0];   \
+		j = keccakf_piln[12]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[12]); t = bc[0];   \
+		j = keccakf_piln[13]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[13]); t = bc[0];   \
+		j = keccakf_piln[14]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[14]); t = bc[0];   \
+		j = keccakf_piln[15]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[15]); t = bc[0];   \
+		j = keccakf_piln[16]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[16]); t = bc[0];   \
+		j = keccakf_piln[17]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[17]); t = bc[0];   \
+		j = keccakf_piln[18]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[18]); t = bc[0];   \
+		j = keccakf_piln[19]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[19]); t = bc[0];   \
+		j = keccakf_piln[20]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[20]); t = bc[0];   \
+		j = keccakf_piln[21]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[21]); t = bc[0];   \
+		j = keccakf_piln[22]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[22]); t = bc[0];   \
+		j = keccakf_piln[23]; bc[0] = st[j];              \
+		st[j] = ROTL64(t, keccakf_rotc[23]); t = bc[0];   \
+	} while (0)
+
+#define CBC(st, bc, j)                                            \
+	do {                                                      \
+		bc[0] = st[j];                                    \
+		bc[1] = st[j + 1];                                \
+		bc[2] = st[j + 2];                                \
+		bc[3] = st[j + 3];                                \
+		bc[4] = st[j + 4];                                \
+	} while (0)
+
+#define CBCST(st, bc, j)                                          \
+	do {                                                      \
+		st[j] ^= (~bc[1]) & bc[2];                        \
+		st[j + 1] ^= (~bc[2]) & bc[3];                    \
+		st[j + 2] ^= (~bc[3]) & bc[4];                    \
+		st[j + 3] ^= (~bc[4]) & bc[0];                    \
+		st[j + 4] ^= (~bc[0]) & bc[1];                    \
+	} while (0)
+
+#define CHI(st, bc)                 \
+	do {                        \
+		CBC(st, bc, 0);     \
+		CBCST(st, bc, 0);   \
+		CBC(st, bc, 5);     \
+		CBCST(st, bc, 5);   \
+		CBC(st, bc, 10);     \
+		CBCST(st, bc, 10);   \
+		CBC(st, bc, 15);     \
+		CBCST(st, bc, 15);   \
+		CBC(st, bc, 20);     \
+		CBCST(st, bc, 20);   \
+	} while (0)
+	
+static void keccakf(uint64_t st[25])
+{
+	int round;
+	uint64_t bc[5];
+
+	for (round = 0; round < KECCAK_ROUNDS; round++) {
+
+		/* Theta */
+		TIT(st, bc);
+		TBC(st, bc);
+
+		/* Rho Pi */
+		RP(st, bc);
+
+		/* Chi */
+		CHI(st, bc);
+
+		/* Iota */
+		st[0] ^= keccakf_rndc[round];
+	}
+}
+
+#else //KECCAKF_MACRO
+
+/* update the state with given number of rounds */
 static void keccakf(uint64_t st[25])
 {
 	int i, j, round;
@@ -69,6 +207,7 @@ static void keccakf(uint64_t st[25])
 		st[0] ^= keccakf_rndc[round];
 	}
 }
+#endif //KECCAKF_MACRO
 
 int sha3_init(struct sha3_ctx *ctx, unsigned int digest_sz)
 {
